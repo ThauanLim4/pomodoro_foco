666dbd6769a65da2460b466b61779f050a17ca82
Thauan Lim'as <116463820+ThauanLim4@users.noreply.github.com>
2024-01-10 11:00:25 -0300

    Pomofocus 1.0v

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 let btn_iniciar = document.getElementById('btn_iniciar');
-let contador = document.getElementById('contador');
+let btn_pausar = document.getElementById('btn_pausar')
+let btn_reset = document.getElementById('btn_reset')
 let settings = document.getElementById('settings');
-let segundos = document.getElementById('segundos');
-let minutos = document.getElementById('minutos');
-let horas = document.getElementById('horas')
 let salvar_config = document.getElementById('btn_salvar_configuracoes')
+let contador = document.getElementById('contador')
+
+let segundos = 0
+let minutos = 0
+let horas = 0
+let milisegundos = 0
+let cron;
 
 const som_play = new Audio('sons/play.wav')
 const beep = new Audio('sons/beep.mp3')
+const pause = new Audio('sons/pause.mp3')
 
 
 btn_iniciar.addEventListener('click', () => {
-    btn_iniciar.innerHTML = 
-    `
-    <h2 id="titulo_comecar">Pausar 
-        <i id="icone_iniciar_pausar" class="bi bi-pause"></i>
-    </h2>
-    `
+    pausar()
     som_play.play()
-    let controle = setInterval(cronometro(), 1000)
+    cron = setInterval(() => { timer(); }, 10)
 })
 
-function cronometro(){
-    if(segundos == 60){
-        segundos -1
-    }
-    if(segundos == 59){
-        minutos++
-        if(segundos < 10){
-            segundos = '0'+segundos
-        }
-        segundos.innerHTML = ':'+segundos
-    }
-    if(minutos == 60){
-        minutos--
-    }
-    if(minutos == 59){
-        horas++
-    }
+btn_pausar.addEventListener('click', () => pausar())
+
+btn_reset.addEventListener('click', function reset() {
+    horas = 0
+    minutos = 0
+    segundos = 0
+    contador.innerHTML = '00:00:00'
+})
+
+function pausar() {
+    clearInterval(cron)
+    pause.play()
 }
 
+function timer() {
+    if((milisegundos += 10) == 1000){
+        milisegundos = 0
+        segundos++
+    }
+    if (segundos == 60) {
+        segundos = 0;
+        minutos++
+    }
+    if (minutos == 60) {
+        minutos = 0
+        horas++
+    }
+    if (minutos == 59) {
+        horas++
+    }
+    contador.innerHTML = zero_a_esquerda(horas) + ':' + zero_a_esquerda(minutos) + ':' + zero_a_esquerda(segundos)
+}
 
+function zero_a_esquerda(input) {
+    return input < 10 ? '0' + input : input;
+}
